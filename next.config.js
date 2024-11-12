6e1b68e69ab54366fa32b0a090c97c3e3c24842c
Shyna <shynahegde@gmail.com>
2024-11-12 02:00:57 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['*'],
-  },
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  }
 }
 
 module.exports = nextConfig
