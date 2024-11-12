87d16406aa990b9eb65f5e864f72562db9dcc82c
Shyna <shynahegde@gmail.com>
2024-11-12 01:13:00 -0600

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import FoodAllergenScanner from '../components/FoodAllergenScanner'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 bg-gray-50">
+      <FoodAllergenScanner />
+    </main>
+  )
+}
