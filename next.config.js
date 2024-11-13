b232fa208b1aec7acb200a24bbfe7aca0cb422f9
Shyna <shynahegde@gmail.com>
2024-11-12 23:51:08 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
   typescript: {
     // Temporarily ignore TypeScript errors during build
     ignoreBuildErrors: true
-  },
-  swcMinify: true
+  }
 }
 
 module.exports = nextConfig
